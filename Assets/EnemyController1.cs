30f8a884bfe7d257a6ee88fbd0a3cb6c99bbbfc8
Riquelmer Añez Jofre <ranez@integratecnologia.es>
2024-05-10 13:44:54 +0200

    TileMap new

--- Assets/EnemyController1.cs
+++ Assets/EnemyController1.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
-public class Fuego : MonoBehaviour
+public class Fueguito : MonoBehaviour
 {
     [SerializeField]private float speed = 3f; // Velocidad de movimiento del enemigo
-    [SerializeField]private float chaseSpeed = 3f;
+    [SerializeField]private float chaseSpeed = 5f;
     [SerializeField]private int health = 3; // Vida del enemigo
     private Rigidbody2D body;
     private Animator anim;
     [SerializeField] private Transform player;
     [SerializeField] private float detectionRadius = 10f;
-    private bool awebo = false;
+
+    private bool push = false;
     private int direction = 1; // Dirección inicial del enemigo (1: derecha, -1: izquierda)
     void Awake(){
         body = GetComponent<Rigidbody2D>();
@@ -29,39 +31,48 @@ public class Fuego : MonoBehaviour
         // Si el enemigo está en estado de persecución, perseguir al jugador
         if (IsOnRadius())
         {
+            anim.SetBool("Chase",true);
             ChasePlayer();
         }
+        else // Si no, realizar la patrulla normal
+        {   
+            
+            anim.SetBool("Chase",false);
+            
+            Patrol();
+            
+        }
         
-        //anim.SetBool("Chasing",IsOnRadius());
+        
     }
-    
+    void Patrol()
+    {
+        
+        transform.Translate(new Vector2(direction,0) * speed * Time.deltaTime);
+        transform.localScale = new Vector3(-direction,1,1);
+        print(true);
+    }
     void ChangeDirection(){
         direction *= -1;
     }
     void ChasePlayer()
     {
-        // Calcula la dirección hacia el jugador
-        Vector2 directionToPlayer = (player.position - transform.position).normalized;
-        if(awebo == false){
-            body.velocity = directionToPlayer * chaseSpeed;
-        }else{
-            ForceApply(10,2,-player.localScale.x);
-            awebo = false;
-        }
-        // Aplica la velocidad dirigida hacia el jugador
+       
         
-
-        // Voltea la escala del sprite según la dirección
-        if (directionToPlayer.x > 0)
+        transform.position = Vector2.MoveTowards(transform.position, player.position, chaseSpeed * Time.deltaTime);
+        Vector3 direccion = player.position-transform.position;
+        if (direccion.x > 0)
         {
             transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
-        else if (directionToPlayer.x < 0)
+        else if (direccion.x < 0)
         {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
         }
+        
+        
+        
     }
-
     bool IsOnRadius(){
         if(player == null){
             return false;
@@ -81,16 +92,18 @@ public class Fuego : MonoBehaviour
         if (other.CompareTag("Attack"))
         {
             // Restar vida al enemigo
-            ForceApply(4,1,-player.localScale.x);
-            //anim.SetTrigger("hit");
+            ForceApply(8,0,-player.localScale.x);
+           
             TakeDamage();
         }
         
     }
     void OnCollisionEnter2D(Collision2D other){
         if (other.gameObject.CompareTag("Player")){
-            ForceApply(10,2,-player.localScale.x);
-            Debug.Log(awebo);
+            anim.SetTrigger("Roll");
+            ForceApply(8,-4,-player.localScale.x);
+            ChasePlayer();
+            
         }else if(other.gameObject.CompareTag("Obstacle")){
             ChangeDirection();
         }
@@ -110,8 +123,7 @@ public class Fuego : MonoBehaviour
     }
     public void ForceApply(int force, int forceUp,float dir){
 
-        //body.velocity = new Vector2(-Mathf.Sign(dir) * force,forceUp);
-        body.AddForce(new Vector2(-Mathf.Sign(dir),0)*force, ForceMode2D.Force);
+        body.velocity = new Vector2(-Mathf.Sign(dir) * force,forceUp);
     }
 }
 
